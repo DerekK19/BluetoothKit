67197b3fe20c253442b3705bdb2e41018c9b6d1c
Derek Knight (mac) <Derek@GordonKnight.co.uk>
2020-06-26 13:34:12 +1200

    Support SPM - Moved all source to Sources folder, added skeleton Tests folder

---
+++ Tests/BluetoothKitTests/BluetoothKitTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import BluetoothKit
+
+final class BluetoothKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BluetoothKit().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
